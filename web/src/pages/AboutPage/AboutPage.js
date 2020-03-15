053a45e074b577054e1665aa69631814af6670c6
David Good <davidagood@gmail.com>
2020-03-14 21:30:22 -0400

    Added blog and contact features

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>This site was created to demonstrate my mastery of Redwood</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
